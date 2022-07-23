0f51dc0041c591c85ae0e59449240f5f80e12074
Evgenii Frolov <evgenii.frolov@smile2impress.com>
2022-07-23 17:59:41 +0300

    fixes

--- src/components/creator/Picker.tsx
+++ src/components/creator/Picker.tsx
@@ -8,14 +8,14 @@ export default function Picker() {
     <div className="content__picker picker">
         
 
-        {Object.entries(ingridients).map( (a, i) => 
-        <PickerItem name={a[0]} 
-        icon={a[0].toLowerCase() + ".png"} 
-        counter={a[1].count} 
+        {Object.entries(ingridients).map( ([first, second], i) => 
+        <PickerItem name={first} 
+        icon={first.toLowerCase() + ".png"} 
+        counter={second.count} 
         key={i}
-        oz={a[1].oz}
-        kcal={a[1].kcal}
-        price={a[1].price}
+        oz={second.oz}
+        kcal={second.kcal}
+        price={second.price}
         />)}
     </div>
   )
